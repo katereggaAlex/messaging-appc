d4d6e2dc0259ff1d1aaaf79a19aa7e5cbf8bdb21
katereggaAlex <77050709+katereggaAlex@users.noreply.github.com>
2021-06-24 12:59:03 +0300

    messaging application

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "app" >
+        <
+        div class Name = "main-body" > <
+        /div> <
+        h1 > This is my first application in react < /h1>  < /
+        div >
+    );
 }
 
 export default App;
