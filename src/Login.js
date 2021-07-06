1afe02e52eee28d8c3c4740631c2220406f49363
katereggaAlex <77050709+katereggaAlex@users.noreply.github.com>
2021-07-06 08:25:38 +0300

    changes

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import "./Login.css"
+import { Button } from '@material-ui/core'
+import { auth, provider } from "./firebase"
 
 function Login() {
-    return ( <
-        div >
-        <
-        h4 > Am the login component < /h4>
 
+    // SignIn Functionality
+    const SignIn = () => {
+        auth.signInWithPopup(provider).then(res => {
+
+        }).catch((error) => alert(error.message))
+    }
+
+    return ( <
+        div className = "login" >
         <
+        div className = "login-container" >
+        <
+        img src = "https://images.unsplash.com/photo-1611606063065-ee7946f0787a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80" / >
+        <
+        div className = "login-text" >
+        <
+        h2 > Sign in to Message Me < /h2> < /
+        div > <
+        Button onClick = { SignIn } > SignIn With Google < /Button> < /
+        div > <
         /div>
     )
 }
--- src/SidebarChats.js
+++ src/SidebarChats.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "./SidebarChats.css"
 import { Avatar, IconButton } from "@material-ui/core"
+import { Link } from "react-router-dom";
 
 function SidebarChats({ channelname, id }) {
     const [randomNumber, setRandom] = useState('')
@@ -10,18 +11,19 @@ function SidebarChats({ channelname, id }) {
         setRandom(Math.floor(Math.random() * 10000))
     }, [])
 
-    return ( <
+    return ( < Link className = "link"
+        to = { '/rooms/${id}' } > <
         div className = "chats" >
         <
         Avatar src = { `https://avatars.dicebear.com/api/human/${randomNumber}.svg` }
         /> <
         div className = "chat-info" >
         <
-        h2 > { channelname } < /h2> <
-        p > Hello guys hows the build < /p> <
-        /div> <
-        /div>
-    )
-}
+        h2 > { channelname } < /h2>            <
+        p > Hello guys hows the build < /p > < /
+        div >
+        <
+        /div> </Link > )
 
-export default SidebarChats
+
+    export default SidebarChats
